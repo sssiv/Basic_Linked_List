f3ea575357155e2fa328f09ed8106c55c85fa7f1
sssiv <67128566+sssiv@users.noreply.github.com>
2021-03-29 14:54:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,57 +2,6 @@
 // Created by SamSS
 //
 #include "list.h"
-#include "queue.h"
-#include "queens.h"
-#include "stack.h"
-#include "chessboard.h"
-
-// std::cout NQueens
-void printQueens()
-{
-	Queens problem1(20);
-	std::cout << problem1 << std::endl;
-    system("pause");
-    system("clear");
-
-	Queens problem2(5);
-	std::cout << problem2 << std::endl;
-    system("pause");
-    system("clear");
-
-	Queens problem3(8);
-	std::cout << problem3 << std::endl;
-    system("pause");
-    system("clear");
-}
-
-// SFML NQueens
-void print_SFML_NQueens()
-{
-	Board board;
-    sf::RenderWindow window({2000, 1250, 32}, "chessboard");
-    sf::RenderStates states;
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-            else if (event.type == sf::Event::KeyPressed)
-            {
-                board.addEvent(event.key.code);
-            }
-        }
-        window.clear();
-        board.makeBoard(1, 1);
-        window.draw(board);
-        window.display();
-    }
-}
 
 // Linked List
 void linked_list()
@@ -70,8 +19,6 @@ void linked_list()
 
 int main()
 {
-    //printQueens();
-	//print_SFML_NQueens();
     linked_list();
 	return 0;
 }
